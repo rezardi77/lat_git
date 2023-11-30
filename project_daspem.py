e7214f7bf9b9761de17f9cfcb8fff9040a500de4
rezardi77 <ardipardede12345@gmail.com>
2023-11-30 20:55:44 +0700

    ini adalah file kedua

---
+++ project_daspem.py
@@ -0,0 +1,9 @@
+#Konsul HALODOGS
+
+print("=================HALO=================")
+print("Selamat Datang di Alodogs")
+print("\nSilahkan isi data diri anda untuk berkonsultasi")
+
+nama_user=input("Masukan Nama Anda : ")
+umur_user=input("Masukan Umur Anda : ")
+jenis_kelamin_user=
